6060868550730b9ae7f5feab10466e93677f46d6
Oliver Schneider <git1984941651981@oli-obk.de>
2015-05-21 18:14:31 +0200

    parsing my first header with libclang

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,45 @@
+#![allow(non_snake_case)]
+
 extern crate libc;
 
-use libc::{c_void, c_int};
+use libc::{c_void, c_int, c_char, c_uint};
+
+pub type CXIndex = *mut c_void;
 
 #[link(name = "clang")]
 extern "C" {
     pub fn clang_createIndex(
         excludeDeclarationsFromPCH: c_int,
         displayDiagnostics: c_int,
-    ) -> *mut c_void;
+    ) -> CXIndex;
+    pub fn clang_parseTranslationUnit(
+        CIdx: CXIndex,
+        source_filename: *const c_char,
+        command_line_args: *const *const c_char,
+        num_command_line_args: c_int,
+        unsaved_files: *const CXUnsavedFile,
+        num_unsaved_files: c_uint,
+        options: c_uint,
+    ) -> CXTranslationUnit;
+}
+
+#[repr(C)]
+pub struct CXTranslationUnitImpl;
+
+pub type CXTranslationUnit = *mut CXTranslationUnitImpl;
+
+#[repr(C)]
+pub struct CXUnsavedFile {
+    Filename: *const c_char,
+    Contents: *const c_char,
+    Length: libc::c_ulong,
+}
+
+#[repr(C)]
+pub enum CXErrorCode {
+    CXError_Success = 0,
+    CXError_Failure = 1,
+    CXError_Crashed = 2,
+    CXError_InvalidArguments = 3,
+    CXError_ASTReadError = 4
 }
--- tests/c_functions.rs
+++ tests/c_functions.rs
@@ -1,8 +1,20 @@
 extern crate clang;
 
 use clang::*;
+use std::ffi::CString;
 
 #[test]
-fn create_index() {
-    let _ = unsafe { clang_createIndex(1, 1) };
+fn parse_header() {
+    let idx = unsafe { clang_createIndex(1, 1) };
+    let filename = CString::new("tests/c_functions.h").unwrap();
+    let tu = unsafe { clang_parseTranslationUnit(
+        idx,
+        filename.as_ptr(),
+        std::ptr::null(),
+        0,
+        std::ptr::null(),
+        0,
+        0,
+    ) };
+    assert!(tu as *const CXTranslationUnitImpl != std::ptr::null());
 }
